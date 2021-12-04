e11df2667ae80725e376b40478137435f48f17bc
kazuya kawaguchi <kawakazu80@gmail.com>
2021-12-05 02:43:38 +0900

    update scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,7 +19,6 @@ if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
-  echo "npmAuthToken: ${NODE_AUTH_TOKEN}" >> ~/.yarnrc.yml
   npm whoami
 fi
 
@@ -27,6 +26,6 @@ fi
 for p in packages/* ; do
   pushd $p
   echo "Publishing $p"
-  pnpm publish --dry-run --access public
+  pnpm publish --access public --no-git-checks
   popd
 done
