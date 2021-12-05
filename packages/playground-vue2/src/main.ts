fe17fea9518e312f5861a5d7155928c798749e07
kazuya kawaguchi <kawakazu80@gmail.com>
2021-12-06 03:17:51 +0900

    update vue-i18n-routing deps

--- packages/playground-vue2/src/main.ts
+++ packages/playground-vue2/src/main.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueCompositionAPI from '@vue/composition-api'
+import { VueI18nRoutingPlugin } from 'vue-i18n-routing'
 
 import App from './App.vue'
 import router from './router'
 
 Vue.use(VueCompositionAPI)
+Vue.use(VueI18nRoutingPlugin as any)
 
 const app = new Vue({
   router,
