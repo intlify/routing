fd1efcec88d46dea0fcc4c55a6c0038c2060cb67
kazuya kawaguchi <kawakazu80@gmail.com>
2021-12-06 02:44:23 +0900

    add playground for vue2

---
+++ packages/playground-vue2/src/main.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+
+import App from './App.vue'
+import router from './router'
+
+Vue.use(VueCompositionAPI)
+
+const app = new Vue({
+  router,
+  render: h => h(App)
+})
+
+app.$mount('#app')
