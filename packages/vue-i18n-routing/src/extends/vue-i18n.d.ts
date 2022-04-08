ece543d64a12272218b84a147a48a84481328821
kazuya kawaguchi <kawakazu80@gmail.com>
2022-04-08 16:07:19 +0900

    more tweak type definitions

--- packages/vue-i18n-routing/src/extends/vue-i18n.d.ts
+++ packages/vue-i18n-routing/src/extends/vue-i18n.d.ts
@@ -1,10 +1,30 @@
 import type { LocaleObject } from '../types'
 import type { ComputedRef } from 'vue-demi'
 
+export interface ComposerCustomProperties {
+  /**
+   * List of locales
+   *
+   * @remarks
+   * Can either be an array of string codes (e.g. `['en', 'fr']`) or an array of {@link LocaleObject} for more complex configurations
+   */
+  locales: ComputedRef<string[] | LocaleObject[]>
+  /**
+   * List of locale codes
+   */
+  localeCodes: ComputedRef<string[]>
+  __baseUrl: string
+}
+declare module 'vue-i18n' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ComposerCustom extends ComposerCustomProperties {}
+}
+
+declare module 'vue-i18n-bridge' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ComposerCustom extends ComposerCustomProperties {}
+}
 declare module '@intlify/vue-i18n-bridge' {
-  export interface ComposerCustom {
-    locales: ComputedRef<string[] | LocaleObject[]>
-    localeCodes: ComputedRef<string[]>
-    __baseUrl: string
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ComposerCustom extends ComposerCustomProperties {}
 }
--- packages/vue-i18n-routing/src/extends/vue-router.d.ts
+++ packages/vue-i18n-routing/src/extends/vue-router.d.ts
@@ -1,24 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// import type { VueRouter, Router } from '@intlify/vue-router-bridge'
 import type { Strategies, Directions } from '../types'
 
+interface VueRouterCustomProperties {
+  __defaultLocale?: string
+  __localeCodes?: string[]
+  __strategy?: Strategies
+  __trailingSlash?: boolean
+  __routesNameSeparator?: string
+  __defaultLocaleRouteNameSuffix?: string
+  __defaultDirection?: Directions
+}
 declare module '@intlify/vue-router-bridge' {
-  interface VueRouter {
-    __defaultLocale?: string
-    __localeCodes?: string[]
-    __strategy?: Strategies
-    __trailingSlash?: boolean
-    __routesNameSeparator?: string
-    __defaultLocaleRouteNameSuffix?: string
-    __defaultDirection?: Directions
-  }
-  interface Router {
-    __defaultLocale?: string
-    __localeCodes?: string[]
-    __strategy?: Strategies
-    __trailingSlash?: boolean
-    __routesNameSeparator?: string
-    __defaultLocaleRouteNameSuffix?: string
-    __defaultDirection?: Directions
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface VueRouter extends VueRouterCustomProperties {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Router extends VueRouterCustomProperties {}
 }
