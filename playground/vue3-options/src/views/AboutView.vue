7c537e163d375a86c6669fea44e4ef295e8d9308
kazuya kawaguchi <kawakazu80@gmail.com>
2022-11-21 12:17:20 +0900

    fix: inherit plugin options (#28)
    * fix: inherit plugin options
    
    * updates

--- playground/vue3-options/src/views/AboutView.vue
+++ playground/vue3-options/src/views/AboutView.vue
@@ -1,3 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  mounted() {
+    console.log('foo', this.$i18n.locales, this.$i18n)
+  }
+})
+</script>
 <template>
   <div class="about">
     <h1>{{ $t('views.AboutView') }}</h1>
